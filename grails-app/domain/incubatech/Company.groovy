9dcc0248cfec3b97f06b7cf97ce68fd68f6f1550
Mairon Passos Ferreira <maironpassos@outlook.com>
2019-02-26 12:21:38 -0300

    add lib pdf

--- grails-app/domain/incubatech/Company.groovy
+++ grails-app/domain/incubatech/Company.groovy
@@ -1,7 +1,7 @@
 package incubatech
 
 class Company {
-    
+
     //Long id_company
     String name
     Integer CNPJ
@@ -12,4 +12,33 @@ class Company {
         CNPJ(nullable: false) 
         email nullable: false, email: true
     }
+
+   /* def toPdf = {
+        def baseUri = request.scheme + "://" + request.serverName + ":" + request.serverPort + grailsAttributes.getApplicationUri(request)
+    
+        // Rendering View
+        def company = Company.get(params.id)
+        def render_result = g.render(template:"/company/pdf.gsp", model: [company: company])
+    
+        // creating PDF
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()
+        //error renderer
+        //ITextRenderer renderer = new ITextRenderer()
+        final def renderer = new ITextRenderer()
+        final ITextFontResolver fontResolver = renderer.getFontResolver();
+
+
+        byte[] b
+        renderer.setDocumentFromString(render_result.toString(), baseUri);
+        renderer.layout()
+        renderer.createPDF(baos)
+        b = baos.toByteArray()
+    
+        // Sending file to the user
+        response.setContentType("application/pdf")
+        response.setHeader("Content-disposition", "attachment; filename=${company.name}.pdf")
+        response.setContentLength(b.length)
+        response.getOutputStream().write(b)
+    }*/
+
 }   
